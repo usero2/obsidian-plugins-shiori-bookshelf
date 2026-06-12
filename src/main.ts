fb7b4b0fc80290b64e70a7f76fbe8c0968e28e83
endofday <gorragid.r@gmail.com>
2026-06-12 08:11:03 +0700

    fix: remove template boilerplate and comments

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import {
-	Editor,
-	MarkdownView,
-	MarkdownFileInfo,
+	App,
 	Modal,
-	Notice,
 	Plugin,
 } from 'obsidian';
 import {
@@ -12,76 +9,13 @@ import {
 	ShioriBookshelfSettingTab,
 } from './settings';
 
-// Remember to rename these classes and interfaces!
-
 export default class ShioriBookshelfPlugin extends Plugin {
 	settings!: ShioriBookshelfSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new ShioriBookshelfModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (
-				editor: Editor,
-				_ctx: MarkdownView | MarkdownFileInfo,
-			) => {
-				editor.replaceSelection('Sample editor command');
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new ShioriBookshelfModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			},
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new ShioriBookshelfSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(activeDocument, 'click', (_evt: MouseEvent) => {
-			new Notice('Click');
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// Sample interval removed
 	}
 
 	onunload() {}
@@ -102,7 +36,7 @@ export default class ShioriBookshelfPlugin extends Plugin {
 class ShioriBookshelfModal extends Modal {
 	onOpen() {
 		const { contentEl } = this;
-		contentEl.setText('Woah!');
+		contentEl.setText('Shiori Bookshelf');
 	}
 
 	onClose() {
--- src/settings.ts
+++ src/settings.ts
@@ -21,18 +21,7 @@ export class ShioriBookshelfSettingTab extends PluginSettingTab {
 		const { containerEl } = this;
 
 		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder('Enter your secret')
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					}),
-			);
+		
+		// Add real settings here when needed
 	}
 }
